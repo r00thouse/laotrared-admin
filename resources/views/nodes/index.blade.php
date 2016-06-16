e873bba4be07df6911de11efdafd91a22682bf93
Sergio Guillen <serguimant@gmail.com>
2016-06-16 19:28:04 -0400

    Display nodes table

--- resources/views/nodes/index.blade.php
+++ resources/views/nodes/index.blade.php
@@ -1,4 +1,43 @@
 @extends('master')
 
 @section('content')
+<table class="table" id="nodes">
+  <thead>
+    <tr>
+      <th>#</th>
+    </tr>
+    <tr>
+      <th>Nombre</th>
+    </tr>
+    <tr>
+      <th></th>
+    </tr>
+    <tr>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+@foreach($nodes as $node)
+    <tr>
+      <td>
+        {{ $node->id }}
+      </td>
+      <td>
+        {{ $node->name }}
+      </td>
+      <td>
+        <a href="/panel/nodos/{{$node->id}}/editar">Editar</a>
+      </td>
+      <td>
+        <a href="#">Borrar</a>
+      </td>
+    </tr>
+@endforeach
+  </tbody>
+</table>
+@endsection
+
+@section('scripts')
+<script type="text/javascript">
+</script>
 @endsection
