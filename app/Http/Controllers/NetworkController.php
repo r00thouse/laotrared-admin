991f20786fb64bb52c0cb16f1ecd0a4ee9fc3a45
Sergio Guillen <serguimant@gmail.com>
2016-08-03 00:02:13 -0400

    Created base controller for network management

---
+++ app/Http/Controllers/NetworkController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Network;
+
+
+class NetworkController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store(Request $r)
+    {
+
+    }
+
+    public function edit($id)
+    {
+
+    }
+
+    public function update(Request $r, $id)
+    {
+
+    }
+
+    public function destroy(Request $r, $id)
+    {
+
+    }
+}
