671f71cd5da4e74e299f51eff825c352cf68c3c7
Sergio Guillen <serguimant@gmail.com>
2016-08-08 23:07:28 -0400

    Added node information on general map

--- resources/views/networkmap.blade.php
+++ resources/views/networkmap.blade.php
@@ -2,8 +2,14 @@
 
 @section('content')
 <div class="row">
-  <div class="col-md-8 col-md-offset-2">
+  <div class="col-md-8">
     <div id="network-map"></div>
+  </div>
+  <div class="col-md-4">
+    <h3>Información de nodo</h3>
+    <div id="info">
+    <p><i>Esta es la lista de nodos actuales, presione alguno para ver información más detallada.</i></p>
+    </div>
   </div>
 </div>
 @endsection
@@ -25,6 +31,16 @@ $(document).ready(function() {
     lat: -16.80454107638344,
     lng: -66.368408203125,
   };
+  var routerIcon = L.icon({
+      iconUrl: '/assets/img/router.png',
+      // shadowUrl: 'leaf-shadow.png',
+
+      iconSize:     [45, 78], // size of the icon
+      shadowSize:   [45, 78], // size of the shadow
+      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+      shadowAnchor: [4, 62],  // the same for the shadow
+      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+  });
 
   var mymap = L.map('network-map').setView([position.lat, position.lng], 5);
   L.tileLayer(tileServer, {
@@ -34,7 +50,23 @@ $(document).ready(function() {
 
   $.ajax('/nodos').then(function(res) {
     res.forEach(function(item, index) {
-      var marker = L.marker([item.latitude, item.longitude]);
+      var marker = L.marker([item.latitude, item.longitude], { icon: routerIcon });
+      marker.data = item;
+      marker.on('click', function() {
+        var infoEl = document.getElementById('info');
+
+        var tpl = '';
+        tpl += '<b>Nombre:</b><br/>' + this.data.name;
+        tpl += '<br/><b>Descripción:</b> <br/><p>';
+        tpl += escape(this.data.description);
+        tpl += '</p><b>Ubicación</b>: <br /><p>';
+        tpl += escape(this.data.physic_description);
+        tpl += '</p><b>Segmento de Red</b>:'
+        tpl += '<br/>IPv4: ' + (this.data.ipv4_range || 'Sin Asignar');
+        tpl += '<br/>IPv6: ' + (this.data.ipv4_range || 'Sin Asignar');
+
+        infoEl.innerHTML = tpl;
+      })
       marker.addTo(mymap);
     });
   });
